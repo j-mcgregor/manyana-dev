870516bcd3be7627d88702f5a9757425a2f8a694
loyd0 <samloyd@hotmail.co.uk>
2022-05-03 14:20:07 +0400

    refactor: 💡 update types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
